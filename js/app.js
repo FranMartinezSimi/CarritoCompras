9f6e4e7c6c7f4bf75d7b213d74f9a457a1c2e9fa
francisco <jua.martinez@outlook.es>
2020-07-12 22:40:13 -0400

    finalizado

--- js/app.js
+++ js/app.js
@@ -14,6 +14,8 @@ function addEventListeners (){
     carrito.addEventListener('click', eliminarCurso);
     //cuando se vacia un carrito
     limpiarCarrito.addEventListener('click', cleanCarrito)
+    //al recargar obtener la data desde el localStorage
+    document.addEventListener('DOMContentLoaded', leerLocalStorage)
     };
 
 //functiones
@@ -55,18 +57,22 @@ function insertarCarrito(curso){
 function eliminarCurso(e){
     e.preventDefault()   
     let curso;
+    let cursoId;
     if(e.target.classList.contains('borrar-curso')){
-        e.target.parentElement.parentElement.remove()
+        e.target.parentElement.parentElement.remove();
+        curso = e.target.parentElement.parentElement;
+        cursoId = curso.querySelector('a').getAttribute('data-id');
     }
-
+    elimninarCursoLocalStorage(cursoId);
 }
 
 //eliminar cursos del carrito en el DOM
 function cleanCarrito(){
     while(cursos.firstChild){
         cursos.removeChild(cursos.firstChild)
-        console.log('limpio')
     }
+    //vaciar LocalStorage
+    vaciarLocalStorage()
     return false
 }
 //almacenar localStorage
@@ -88,4 +94,45 @@ function obtenerCursosLocalStorage(){
         cursosLS = JSON.parse(localStorage.getItem('cursos'));
     }
     return cursosLS;
+}
+
+//imprime los cursos desde localStorage en el carrito
+
+function leerLocalStorage(){
+    let cursosLS;
+    cursosLS = obtenerCursosLocalStorage();
+    cursosLS.forEach(curso => {
+        //construir template
+        const row = document.createElement('tr');
+        row.innerHTML =`
+        <td> 
+            <img src="${curso.imagen}" width=100 />
+        </td>
+        <td>${curso.titulo}<td/>
+        <td>${curso.presio}<td/>
+        <td>
+            <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
+        <td/>`
+        cursos.appendChild(row)
+    });
+}
+
+//eliminar curso por ID en localStorage
+function elimninarCursoLocalStorage(curso){
+    let cursosLS;
+    //obtener todo el arreglo del curso
+    cursosLS = obtenerCursosLocalStorage();
+    //iterar comprarando el ID del curso borrado en localStorage
+    cursosLS.forEach(cursoLS => {
+        if(cursoLS.id=== curso){
+            cursosLS.splice(index, 1);
+        }
+    });
+    //add el arreglo actual a storage
+   localStorage.setItem('cursos', JSON.stringify(cursosLS))
+}
+
+//eliminar todos los cursos de localStorage
+function vaciarLocalStorage(){
+    localStorage.clear()
 }
